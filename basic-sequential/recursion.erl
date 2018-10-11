f052c51be9d7e73b90b75680132cb2bf04d78b1f
Oleksandr Zinchenko <zinchenko@live.com>
2018-10-10 22:13:54 -0700

    a few example of recursion for getting an average of the list

--- basic-sequential/recursion.erl
+++ basic-sequential/recursion.erl
@@ -1,6 +1,29 @@
 -module(recursion).
--export([bump/1]).
+-export([ bump/1
+        , average/1
+        , sum/1
+        , len/1
+        , better_average/1]).
 
+% -------------------------
 % bumping list of integers.
 bump([]) -> [];
 bump([Head | Tail]) -> [Head + 1 | bump(Tail)].
+
+% -------------------------
+% a few examples of getting an average value of an integer list.
+average(Lst) -> sum(Lst) / len(Lst).
+
+sum([]) -> 0;
+sum([H|T]) -> H + sum(T).
+
+len([]) -> 0;
+len([_|T]) -> 1 + len(T).
+
+better_average([]) -> {error, "Empty list"};
+better_average(Lst) ->
+    {Sum, Len} = foldl_average(Lst, 0, 0),
+    Sum / Len.
+
+foldl_average([], S, L)    -> {S, L};
+foldl_average([H|T], S, L) -> foldl_average(T, S+H, L+1).
