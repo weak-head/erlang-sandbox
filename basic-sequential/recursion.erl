dd15cd57864c104920e43b692f038a549889f7dc
Oleksandr Zinchenko <zinchenko@live.com>
2018-10-10 22:52:31 -0700

    tail recursion with accumulator

--- basic-sequential/recursion.erl
+++ basic-sequential/recursion.erl
@@ -5,7 +5,9 @@
         , len/1
         , better_average/1
         , even/1
-        , member/2]).
+        , member/2
+        , sum_t/1
+        , bump_t/1]).
 
 % -------------------------
 % bumping list of integers.
@@ -41,3 +43,19 @@ even([_|T]) -> even(T).
 member(_, [])    -> false;
 member(H, [H|_]) -> true;
 member(H, [_|T]) -> member(H, T).
+
+% --------------------------
+% tail-recursion
+sum_t([]) -> 0;
+sum_t(L) -> sum_acc(L,0).
+
+sum_acc([], N) -> N;
+sum_acc([H|T], N) -> sum_acc(T, N+H).
+
+bump_t(L) -> reverse_acc(bump_acc(L, []), []).
+
+bump_acc([], A)    -> A;
+bump_acc([H|T], A) -> bump_acc(T, [H+1 | A]).
+
+reverse_acc([], A)    -> A;
+reverse_acc([H|T], A) -> reverse_acc(T, [H | A]).
