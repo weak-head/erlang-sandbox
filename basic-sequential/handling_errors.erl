fb8d08bc748e5e82178f4357c0ec6c2e9dcf1bbf
Oleksandr Zinchenko <zinchenko@live.com>
2018-10-11 22:26:06 -0700

    additional try catch example

--- basic-sequential/handling_errors.erl
+++ basic-sequential/handling_errors.erl
@@ -1,5 +1,5 @@
 -module(handling_errors).
--export([test/1]).
+-export([test/1, return_error/1, try_return/1]).
 
 
 % 43
@@ -33,4 +33,33 @@ test(fourth) ->
         Val -> {normal, Val}
     catch
         throw:Error -> {throw,Error}
+    end.
+
+
+return_error(X) when X < 0 ->
+    throw({'EXIT', {badarith, [{exception,return_error,1},
+                               {erl_eval,do_apply,5},
+                               {shell,exprs,6},
+                               {shell,eval_exprs,6},
+                               {shell,eval_loop,3}]}});
+return_error(X) when X == 0 ->
+    1/X;
+return_error(X) when X > 0 ->
+    {'EXIT', {badarith, [{exception,return_error,1},
+                         {erl_eval,do_apply,5},
+                         {shell,exprs,6},
+                         {shell,eval_exprs,6},
+                         {shell,eval_loop,3}]}}.
+
+% three different kinds of behavior:
+% try_return(1).
+% try_return(0).
+% try_return(-1).
+try_return(X) when is_integer(X) ->
+    try return_error(X) of
+        Val -> {normal, Val}
+    catch
+        exit:Reason -> {exit, Reason};
+        throw:Throw -> {throw, Throw};
+        error:Error -> {error, Error}
     end.
