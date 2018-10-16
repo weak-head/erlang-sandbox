bc6e1509528c3ba7dcd0f0adb21051056359052f
Oleksandr Zinchenko <zinchenko@live.com>
2018-10-15 22:23:04 -0700

    basic side effects and recursion

--- basic-sequential/playground.erl
+++ basic-sequential/playground.erl
@@ -1,5 +1,6 @@
 -module(playground).
--export([sum/1, sum/2, create/1, reverse_create/1]).
+-export([sum/1, sum/2, create/1, reverse_create/1, print_ints/1, print_even_ints/1]).
+-import(io, [format/2]).
 
 sum(N) when N =< 0 -> 0;
 sum(N) -> N + sum(N-1).
@@ -23,3 +24,19 @@ reverse_create(N) when N > 0 ->
     [N|reverse_create(N-1)];
 reverse_create(N) when N < 0 ->
     throw({invalid_input}).
+
+print_ints(N) when N < 0 ->
+    throw(invalid_input);
+print_ints(0) -> {done};
+print_ints(N) ->
+    io:format("Number:~p~n",[N]),
+    print_ints(N-1).
+
+print_even_ints(N) when N < 0 ->
+    throw(invalid_input);
+print_even_ints(0) -> {done};
+print_even_ints(N) when N rem 2 == 0 ->
+    io:format("Number:~p~n",[N]),
+    print_even_ints(N-2);
+print_even_ints(N) when N rem 2 /= 0 ->
+    print_even_ints(N-1).
