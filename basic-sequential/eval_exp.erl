696679fb637194a587374d2a1a29c84a1da778d8
Oleksandr Zinchenko <zinchenko@live.com>
2018-10-21 16:16:13 -0700

    tokenizer and lexer for simple arith expression

--- basic-sequential/eval_exp.erl
+++ basic-sequential/eval_exp.erl
@@ -1,5 +1,5 @@
 -module(eval_exp).
--export([next_token/1]).
+-export([next_token/1, tokenize/1]).
 
 % parse simple arithmetic expression
 % Expr:
@@ -8,21 +8,37 @@
 %   {minus, {plus, {num, 2}, {num, 3}}, {num, 4}}
 parse([]) -> [].
 
-next_token([]) -> {{eof}, []};
+
+% tokenize simple arithmetic expression
+% Expr:
+%   (23+42)-4
+% Into:
+%   [open_parenthesis, {num,23}, plus, {num,42}, close_parenthesis, minus, {num,4}]
+tokenize([])  -> [];
+tokenize(Lst) ->
+    NextToken = next_token(Lst),
+    case NextToken of
+        {error, Reason} -> [{error, Reason}];
+        {{eof}, []}     -> [];
+        {Token, Rest}   -> [Token|tokenize(Rest)]
+    end.
+
+
+next_token([])    -> {{eof}, []};
 next_token([H|T]) ->
     case [H] of
         " " -> next_token(T);
-        "(" -> {{open_parenthesis}, T};
-        ")" -> {{close_parenthesis}, T};
-        "{" -> {{open_brace}, T};  % curly bracket
-        "}" -> {{close_brace}, T}; % curly bracket
-        "[" -> {{open_bracket}, T};
-        "]" -> {{close_bracket}, T};
-        "-" -> {{minus}, T};
-        "+" -> {{plus}, T};
+        "(" -> {open_parenthesis, T};
+        ")" -> {close_parenthesis, T};
+        "{" -> {open_brace, T};  % curly bracket
+        "}" -> {close_brace, T}; % curly bracket
+        "[" -> {open_bracket, T};
+        "]" -> {close_bracket, T};
+        "-" -> {minus, T};
+        "+" -> {plus, T};
         _   -> case string:to_integer([H|T]) of
-                {Int, Rest} -> {{num, Int}, Rest};
-                {error, _}  -> {error, unknown_token}
+                {error, _}  -> {error, unknown_token};
+                {Int, Rest} -> {{num, Int}, Rest}
                end
     end.
 
