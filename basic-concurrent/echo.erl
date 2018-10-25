72b0414721a0de68bdaffb8e784540642311522e
Oleksandr Zinchenko <zinchenko@live.com>
2018-10-24 22:54:08 -0700

    echo using pid alias

--- basic-concurrent/echo.erl
+++ basic-concurrent/echo.erl
@@ -1,5 +1,6 @@
 -module(echo).
 -export([go/0, loop/0]).
+-export([go_r/0, loop_r/0]).
 
 go() ->
     Pid = spawn(echo, loop, []),
@@ -18,3 +19,24 @@ loop() ->
         stop ->
             true
     end.
+
+% ---------------
+
+go_r() ->
+    register(echo, spawn(echo, loop_r, [])),
+    echo ! {self(), hello},
+    receive
+        {_Pid, Msg} ->
+            io:format("~w~n", [Msg])
+    end,
+    echo ! stop.
+
+loop_r() ->
+    receive
+        {From, Msg} ->
+            From ! {self(), Msg},
+            loop_r();
+        stop ->
+            true
+    end.
+
