4eaaf747f2f96cb1ce0b464613b540ab76d15647
Oleksandr Zinchenko <zinchenko@live.com>
2018-10-25 22:20:42 -0700

    straingforward example of seand_after usage

--- basic-concurrent/simple_timer.erl
+++ basic-concurrent/simple_timer.erl
@@ -1,5 +1,5 @@
 -module(simple_timer).
--export([send_after/2, sleep/1, send/3]).
+-export([send_after/2, sleep/1, send/3, ping/1]).
 
 send_after(Time, Msg) ->
     spawn(simple_timer, send, [self(), Time, Msg]).
@@ -14,4 +14,14 @@ sleep(Time) ->
     receive
     after Time ->
         true
+    end.
+
+% --------------
+
+ping(WaitTime) ->
+    send_after(100, pong),
+    receive
+        pong -> ping_pong
+    after
+        WaitTime -> nope
     end.
