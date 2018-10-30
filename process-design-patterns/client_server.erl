4dcd8cd77622ab3d226b085f3a748066166fd340
Oleksandr Zinchenko <zinchenko@live.com>
2018-10-29 23:15:45 -0700

    client-server process desing pattern

---
+++ process-design-patterns/client_server.erl
@@ -0,0 +1,50 @@
+-module(client_server).
+-export([start/0, stop/0, allocate/0, deallocate/1]).
+-export([init/0]).
+
+start() ->
+    register(client_server, spawn(client_server, init, [])).
+
+stop()           -> call(stop).
+allocate()       -> call(allocate).
+deallocate(Freq) -> call({deallocate, Freq}).
+
+% ----
+
+init() ->
+    Frequencies = {get_frequencies(), []},
+    loop(Frequencies).
+
+call(Msg) ->
+    client_server ! {request, self(), Msg},
+    receive
+        {reply, Reply} -> Reply
+    end.
+
+loop(Frequencies) ->
+    receive
+        {request, Pid, allocate} ->
+            {NewFrequencies, Reply} = allocate(Frequencies, Pid),
+            reply(Pid, Reply),
+            loop(NewFrequencies);
+        {request, Pid, {deallocate, Freq}} ->
+            NewFrequencies = deallocate(Frequencies, Freq),
+            reply(Pid, ok),
+            loop(NewFrequencies);
+        {request, Pid, stop} ->
+            reply(Pid, ok)
+    end.
+
+allocate({[], Allocated}, _Pid) ->
+    {{[], Allocated}, {error, no_frequency}};
+allocate({[Freq|Free], Allocated}, Pid) ->
+    {{Free, [{Freq, Pid}|Allocated]}, {ok, Freq}}.
+
+deallocate({Free, Allocated}, Freq) ->
+    NewAllocated=list:keydelete(Freq, 1, Allocated),
+    {[Freq|Free], NewAllocated}.
+
+reply(Pid, Reply) ->
+    Pid ! {reply, Reply}.
+
+get_frequencies() -> [10,11,12,13,14,15].
