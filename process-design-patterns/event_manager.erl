94430fed22c71997bd97881cfb0235b048bbbb06
Oleksandr Zinchenko <zinchenko@live.com>
2018-11-05 21:53:01 -0800

    finalizing event manager

--- process-design-patterns/event_manager.erl
+++ process-design-patterns/event_manager.erl
@@ -21,12 +21,60 @@ terminate([]) -> [];
 terminate([{Handler, Data}|Rest]) ->
     [{Handler, Handler:terminate(Data)}|terminate(Rest)].
 
-add_handler(Name, Handler, InitData) -> ok.
 
-delete_handler(Name, Handler) -> ok.
+add_handler(Name, Handler, InitData) ->
+    call(Name, {add_handler, Handler, InitData}).
 
-get_data(Name, Handler) -> ok.
+delete_handler(Name, Handler) ->
+    call(Name, {delete_handler, Handler}).
 
-send_event(Name, Event) -> ok.
+get_data(Name, Handler) ->
+    call(Name, {get_data, Handler}).
 
-loop(State) -> ok.
+send_event(Name, Event) ->
+    call(Name, {send_event, Event}).
+
+
+handle_msg({add_handler, Handler, InitData}, LoopData) ->
+    {ok, [{Handler, Handler:init(InitData)}|LoopData]};
+
+handle_msg({delete_handler, Handler}, LoopData) ->
+    case lists:keysearch(Handler, 1, LoopData) of
+        false ->
+            {{error, instance}, LoopData};
+        {value, {Handler, Data}} ->
+            Reply = {data, Handler:terminate(Data)},
+            NewLoopData = lists:keydelete(Handler, 1, LoopData),
+            {Reply, NewLoopData}
+    end;
+
+handle_msg({get_data, Handler}, LoopData) ->
+    case lists:keysearch(Handler, 1, LoopData) of
+        false                    -> {{error, instance}, LoopData};
+        {value, {Handler, Data}} -> {{data, Data}, LoopData}
+    end;
+
+handle_msg({send_event, Event}, LoopData) ->
+    {ok, event(Event, LoopData)}.
+
+
+event(_Event, []) -> [];
+event(Event, [{Handler, Data}|Rest]) ->
+    [{Handler, Handler:handle_event(Event, Data)}|event(Event, Rest)].
+
+call(Name, Msg) ->
+    Name ! {request, self(), Msg},
+    receive {reply, Reply} -> Reply end.
+
+reply(To, Msg) ->
+    To ! {reply, Msg}.
+
+loop(State) ->
+    receive
+        {request, From, Msg} ->
+            {Reply, NewState} = handle_msg(Msg, State),
+            reply(From, Reply),
+            loop(NewState);
+        {stop, From} ->
+            reply(From, terminate(State))
+    end.
