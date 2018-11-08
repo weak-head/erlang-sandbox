e36ff48cf72e8639c11e2ebe593a7baeed77f737
Oleksandr Zinchenko <zinchenko@live.com>
2018-11-07 23:00:16 -0800

    splitting db api into public and private parts

--- process-design-patterns/sandbox/db.erl
+++ process-design-patterns/sandbox/db.erl
@@ -1,18 +1,50 @@
 -module(db).
 -export([start/0, stop/0, write/2, delete/1, read/1, match/1]).
 
-% ---
+% --- Public API
 
-start() -> ok.
+start() ->
+    register(db, spawn(db, init, [])).
 
-stop() -> ok.
+stop() ->
+    call(stop).
 
-write(Key, Element) -> ok.
+write(Key, Element) ->
+    call({write, Key, Element}).
 
-delete(Key) -> ok.
+delete(Key) ->
+    call({delete, Key}).
 
-read(Key) -> {ok, element}.
+read(Key) ->
+    call({read, Key}).
 
-match(Element) -> [key1, key2].
+match(Element) ->
+    call({match, Element}).
 
-% ---
+
+% --- Private generic API
+
+init() ->
+    loop([]).
+
+loop(State) ->
+    receive
+        {request, From, Msg} ->
+            {Reply, NewState} = handle_request(Msg, State),
+            reply(From, Reply),
+            loop(NewState);
+        {stop, From} ->
+            reply(From, ok)
+    end.
+
+call(Msg) ->
+    db ! {request, self(), Msg},
+    receive {reply, Response} -> Response end.
+
+reply(To, Msg) ->
+    To ! {reply, Msg}.
+
+
+% --- Request handlers
+
+handle_request({write, Key, Element}, State) -> ok.
