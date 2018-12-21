c36ed12b0893fdb2ef6812be3ad6a5b3c3fdc313
Oleksandr Zinchenko <zinchenko@live.com>
2018-12-20 22:50:56 -0800

    Heartbeat between two processes

---
+++ process-error-handling/heartbeat.erl
@@ -0,0 +1,56 @@
+-module(heartbeat).
+-export([start/0, stop/0, status/0]).
+-export([init/0, entry_point/0, main_loop/2]).
+
+% -------------------------
+
+start() ->
+    init(),
+    {ok}.
+
+stop() ->
+    {ok}.
+
+status() ->
+    {running}.
+
+% -------------------------
+
+init() ->
+    RPID = spawn(heartbeat, entry_point, []),
+    LPID = spawn(heartbeat, entry_point, []),
+    register(heartbeat_r, RPID),
+    register(heartbeat_l, LPID),
+    heartbeat_r ! {attach, LPID},
+    heartbeat_l ! {attach, RPID}.
+
+entry_point() ->
+    receive
+        {attach, PID} ->
+            main_loop(PID, false)
+    end.
+
+main_loop(PID, Waiting) ->
+    receive
+        {request, ping} ->
+            PID ! {response, pong},
+            main_loop(PID, Waiting);
+        {response, pong} ->
+            case Waiting of
+                false ->
+                    throw({unsynced_instance});
+                true ->
+                    io:format("~w~w~n", [self(), ' pong']),
+                    main_loop(PID, false)
+            end
+    after
+        1000 ->
+            case Waiting of
+                true ->
+                    throw({dead_couple});
+                false ->
+                    PID ! {request, ping},
+                    io:format("~w~w~n", [self(), ' ping']),
+                    main_loop(PID, true)
+            end
+    end.
